4edd592872f76fc3211f23284c4129ef86b42f9d
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-02-21 13:19:18 -0600

    Add files via upload

---
+++ skeletoncode/clicks.py
@@ -0,0 +1,84 @@
+import pyautogui as pg
+import time
+import random
+import win32con, win32api
+import keyboard
+from win32gui import FindWindow, GetWindowRect
+
+
+class Randomize():
+    def __init__(self,minMaxs):
+        self.minMaxs = [minMaxs] # (min_x, max_x, min_y, max_y)
+        self.x_min = minMaxs[0]
+        self.x_max = minMaxs[1]
+        self.y_min = minMaxs[2]
+        self.y_max = minMaxs[3]
+        self.random_pix_move_y = random.randrange(25,27,1)
+        self.random_pix_move_x = random.randrange(-40,40,1)
+
+        self.random_multi = random.randrange(30,50,1)/100
+
+        self.random_sleep= random.randrange(30,40,1)/100
+        self.random_sleep2= random.randrange(10,30,1)/100
+        self.random_sleep3= random.randrange(30,50,1)/1000
+        self.random_long_sleep = random.randrange(20,50,1)/100
+        self.new_x = random.randrange(self.x_min, self.x_max+1)
+        self.new_y = random.randrange(self.y_min, self.y_max+1)
+
+    def shiftclick(self):
+        keyboard.press('shift')
+        pg.moveTo(self.new_x,self.new_y, duration=.25)
+        time.sleep(.05)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, self.new_x, self.new_y, 0, 0)
+        time.sleep(.05)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, self.new_x, self.new_y, 0, 0)
+        time.sleep(.08)
+        keyboard.release('shift')
+
+    def move(self):
+        pg.moveTo(self.new_x,self.new_y, duration=self.random_sleep2)
+
+    
+    def randleftspeed(self):
+        pg.moveTo(self.new_x,self.new_y, duration=.3)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, self.new_x, self.new_y, 0, 0)
+        time.sleep(.02)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, self.new_x, self.new_y, 0, 0)
+        time.sleep(.02)
+    
+    
+    def randleft(self):
+        pg.moveTo(self.new_x,self.new_y, duration=.1)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, self.new_x, self.new_y, 0, 0)
+        time.sleep(.02)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, self.new_x, self.new_y, 0, 0)
+        time.sleep(.03)
+    
+    
+    def dragmove(self,x,y):
+        pg.moveTo(self.new_x,self.new_y, duration=.3)
+        time.sleep(.1)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, self.new_x, self.new_y, 0, 0)
+        pg.moveTo(x, y, duration=.6)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+        time.sleep(.1)
+
+def findobj(objsRGBVal,screen):
+        flag=0
+        #screen shots location cords passed
+        ss = pg.screenshot(region=screen)
+        #loops through screen shot region from left to right by 5 pixel increments
+        for y in range(screen[1], screen[3], 5):
+            for x in range(screen[0], screen[2], 5):
+                #red, green, blue at each x,y cord
+                rgb = ss.getpixel((x, y))
+                #checks if rbg value == the color rgb value passed through
+                if rgb==objsRGBVal:
+                    flag = 1
+                    new_x= x
+                    new_y= y
+                    time.sleep(0.05)
+                    break
+            #if rgb found save x,y cords and send back                
+            if flag == 1:
+                    return new_x,new_y
---
+++ skeletoncode/pointer.py
@@ -0,0 +1,17 @@
+import pyautogui as pg
+import keyboard
+import sys
+pg.moveTo(848,568)
+x=pg.displayMousePosition()
+
+"""while True:
+    if keyboard.is_pressed('q'):
+        sys.exit()
+    center=pg.pixel(821,528)
+    if center == (0,2,0):
+        print('True')
+    elif center != (0,2,0):
+        print('False')"""
+
+#120,0,139
+#255,3,255
