eeccb7d4fd4b190e9bb13e101a2c0a22899c5808
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-03-23 13:55:26 -0500

    Update testing.py
    more control

--- mining/testing.py
+++ mining/testing.py
@@ -1,11 +1,11 @@
-def findobjat(objsRGBVal,coords=(0,0,1080,670),step=1):#topleftx, toplefty, bottomrightx, bottomrighty
+def findobjat(objsRGBVal,coords=(0,0,1080,670),stepleftright=1,stepupdown=1):#topleftx, toplefty, bottomrightx, bottomrighty
     flag=0
     wincap = WindowCapture('RuneLite')
     screenshot=pg.screenshot('test.png',region=(wincap.coords[0],wincap.coords[1],coords[2],coords[3]))
     
     
-    for y in range(coords[1], coords[3], step):#1,3
-        for x in range(coords[0], coords[2], step):#0,2
+    for y in range(coords[1], coords[3], stepupdown):#1,3
+        for x in range(coords[0], coords[2], stepleftright):#0,2
             rgb = screenshot.getpixel((x, y))
             if rgb==objsRGBVal:
                 flag = 1
